3cfc69b6bf32346c48c894310625e8a7879a1da6
Finn <32446445+Delafinn@users.noreply.github.com>
2021-11-15 20:27:47 -0700

    Update coffee.py

--- coffee.py
+++ coffee.py
@@ -15,7 +15,7 @@ coffeequestion = input(name1 + " do you like coffee?") # making a while loop
 while (coffeequestion != "yes" and coffeequestion != "no"):
     print(name1 + "I'm gonna grab a coffee. \n   You are more than welcome to join me to the coffee shop.")
     time.sleep(3)
-    coffeequestion = input(name1 + " but really do you like coffee?")
+    coffeequestion = input(name1 + " are you joining?")
 
 if coffeequestion == "yes"  :
     print(name1 + " lets get some coffee. \n  I'm heading to the cofee shop now.")
