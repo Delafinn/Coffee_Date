43cdc18e4746d1f2953247e36b34958cdbbe0e5e
Finn <32446445+Delafinn@users.noreply.github.com>
2021-11-15 20:17:41 -0700

    Update coffee.py

--- coffee.py
+++ coffee.py
@@ -1,22 +1,29 @@
 import time
 
+
 name1 = input("what is your name?") # asking the user their name
-print("what a nice name")
+if name1 == "neo" :
+    print("Mr.Anderson.")
+if name1 == "dude" :
+    print("sah dude!")
+else :
+    print("what a nice name")
 time.sleep(1)
 
-coffeequestion = input(name1 + " do you like coffee?")
+coffeequestion = input(name1 + " do you like coffee?") # making a while loop
 
-if coffeequestion == " yes"  :
-    print(name1 + " lets get some coffee.")
+while (coffeequestion != "yes" and coffeequestion != "no"):
+    print(name1 + "I'm gonna grab a coffee. \n   You are more than welcome to join me to the coffee shop.")
+    time.sleep(3)
+    coffeequestion = input(name1 + " but really do you like coffee?")
+
+if coffeequestion == "yes"  :
+    print(name1 + " lets get some coffee. \n  I'm heading to the cofee shop now.")
     time.sleep(2)
+    fq = input("what's your favorite drink?")
 
-elif coffeequestion == " no" :
+
+if coffeequestion == "no" :
     print(name1 + " I'm gonna grab a coffee. \n   You are more than welcome to join me to the coffee shop")
     time.sleep(3)
-    secondquestion = input(name1 + " are you coming?")
-
-
-else:
-    print(name1 + " I'm gonna grab a coffee. \n   You are more than welcome to join me to the coffee shop")
-    time.sleep(3)
-    secondquestion = input(name1 + " are you coming?")
+    sq = input(name1 + " are you joining?")
