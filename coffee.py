957971fc91baa6dd0c6874cf65641198f721b41a
Finn <32446445+Delafinn@users.noreply.github.com>
2021-12-06 13:22:10 -0700

    new coffee version 1.2
    1.2

--- coffee.py
+++ coffee.py
@@ -1,10 +1,17 @@
 import time # used to slow down dialogue
+import sys # used for quitting
+
 while True: # Main loop
-    name1 = input("what is your name?") # asking the user their name
+
+
+
+    name1 = input("what is your name? to exit the program type quit at any time") # asking the user their name
     if name1 == "neo" :
         print("Mr.Anderson.")
     if name1 == "dude" :
         print("sah dude!")
+    if name1 == ("quit"):
+        sys.exit("quitting program") # quitting
     else :
         print("what a nice name")
     time.sleep(2)
@@ -12,22 +19,29 @@ while True: # Main loop
     coffeequestion = input(name1 + " do you like coffee? type either (y)es or (n)o ")
 
     while (coffeequestion != "yes" and coffeequestion != "y" and coffeequestion != "no" and coffeequestion != "n"): # loop if user doesn't say yes or no.
-        coffeequestion = input(name1 + "  Really do you like coffee?")
+        coffeequestion = input(name1 + " do you like coffee?")
 
     if coffeequestion == "yes" or coffeequestion ==  "y":
         print(name1 + " Lets get some coffee.")
         time.sleep(2)
         fq = input("What's your favorite coffee drink?")
         if fq == "americano":
-                print("That's my favorite coffee too!")
+            print("That's my favorite coffee too!")
+        if fq == ("quit"):
+            sys.exit("quitting program") # quitting
         else:
-                print("nice!")
+            print("nice!")
 
     if coffeequestion == "no" or coffeequestion == "n":
         print(name1 + " I'm gonna grab a coffee. \n   You are more than welcome to join me to the coffee shop")
         time.sleep(3)
         sq = input(name1 + " Are you joining? type either (y)es or (n)o")
         if sq == "no" or sq == "n":
-                print("see you soon!")
+            print("see you soon!")
         if sq == "yes" or sq ==  "y":
-                print("sweet!")
+            print("sweet!")
+        if sq == ("quit"):
+            sys.exit("quitting program") # quitting
+
+    if coffeequestion == ("quit"):
+        sys.exit("quitting program")  # quitting
